61049b8d0d13ecf126efc056727dc90db979c65d
Nameless <admin@4otaku.ru>
2012-03-17 23:29:16 +0400

    init

---
+++ boot/init.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once 'constants.php';
+include_once 'autoloader.php';
+include_once 'error_handler.php';
+
+mb_internal_encoding('UTF-8');
+date_default_timezone_set('Europe/Moscow');
+
+Autoload::init(LIBS);
+
+echo "123";
