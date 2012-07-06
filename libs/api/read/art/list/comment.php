66a438adf1e042dd5131ba9dfd841bfdaf5f571a
Nameless <admin@4otaku.ru>
2012-07-06 18:08:30 +0400

    workan

--- libs/api/read/art/list/comment.php
+++ libs/api/read/art/list/comment.php
@@ -1,6 +1,6 @@
 <?php
 
-class Api_Read_Art_List_Pack_Comment extends Api_Read_Art_List_Art
+class Api_Read_Art_List_Comment extends Api_Read_Art_List
 {
 	protected $fields = array('id', 'id_parent', 'md5', 'animated');
 }
