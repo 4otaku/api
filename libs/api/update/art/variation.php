93b105b58653a7d7e25e7e4f50fb82db55ee749a
Nameless <admin@4otaku.ru>
2013-05-12 00:09:38 +0400

    edit variations list

--- libs/api/update/art/variation.php
+++ libs/api/update/art/variation.php
@@ -9,9 +9,40 @@ class Api_Update_Art_Variation extends Api_Update_Abstract
 		$id = $this->get('id');
 		$add = (array) $this->get('add');
 		$remove = (array) $this->get('remove');
+		$order = (array) $this->get('order');
 
-		if (empty($id) || (empty($add) && empty($remove))) {
+		if (empty($id) || (empty($add) && empty($remove) && empty($order))) {
 			throw new Error_Api(Error_Api::MISSING_INPUT);
+		}
+
+		$actual_order = array();
+		$previous_order = array_keys($this->db->order('id_parent_order', 'asc')
+			->get_vector('art', 'id', 'id_parent = ?', $id));
+		foreach ($order as $item) {
+			if (in_array($item, $previous_order) && !in_array($item, $remove)) {
+				array_push($actual_order, $item);
+			}
+		}
+		foreach ($previous_order as $item) {
+			if (!in_array($item, $actual_order) && !in_array($item, $remove)) {
+				array_push($actual_order, $item);
+			}
+		}
+		$id = reset($actual_order);
+
+		foreach ($remove as $item) {
+			try {
+				$this->do_delete($id, $item);
+			} catch (Error_Api $e) {
+				$this->add_error($e->getCode(), $e->getMessage());
+			}
+		}
+
+		foreach ($actual_order as $pos => $item) {
+			$this->db->update('art', array(
+				'id_parent' => $id,
+				'id_parent_order' => $pos,
+			), (int) $item);
 		}
 
 		foreach ($add as $item) {
@@ -22,14 +53,7 @@ class Api_Update_Art_Variation extends Api_Update_Abstract
 			}
 		}
 
-		foreach ($remove as $item) {
-			try {
-				$this->do_delete($id, $item);
-			} catch (Error_Api $e) {
-				$this->add_error($e->getCode(), $e->getMessage());
-			}
-		}
-
+		$this->add_answer('parent', $id);
 		$this->set_success(true);
 	}
 
@@ -62,8 +86,8 @@ class Api_Update_Art_Variation extends Api_Update_Abstract
 		}
 
 		$this->db->update('art', array(
-			'id_parent' => $data['id'],
+			'id_parent' => $data,
 			'id_parent_order' => 0,
-		), (int) $data['id']);
+		), $data);
 	}
 }
