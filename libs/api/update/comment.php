dba838529ac5a91d473fb80d857f4d3c95840466
Nameless <admin@4otaku.ru>
2013-05-15 21:09:45 +0400

    fix

---
+++ libs/api/update/comment.php
@@ -0,0 +1,25 @@
+<?php
+
+class Api_Update_Comment extends Api_Update_Abstract
+{
+	public function process()
+	{
+		$id = (int) $this->get('id');
+		$text = (string) $this->get('text');
+		$text = trim($text);
+
+		if (empty($text) || empty($id)) {
+			throw new Error_Api(Error_Api::MISSING_INPUT);
+		}
+
+		if (!$this->is_moderator()) {
+			throw new Error_Api(Error_Api::INSUFFICIENT_RIGHTS);
+		}
+
+		$this->db->update('comment', array(
+			'text' => $text,
+		), $id);
+
+		$this->set_success(true);
+	}
+}
