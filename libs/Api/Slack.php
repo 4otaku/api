b4715b574873e56632e69dd73745bd643adf49a7
Konstantin Pakshaev <derinov@odesk.com>
2015-10-18 11:56:32 +0200

    slack api checking for links and commands

--- libs/Api/Slack.php
+++ libs/Api/Slack.php
@@ -4,11 +4,18 @@ namespace Otaku\Api;
 
 class ApiSlack extends ApiAbstract
 {
+    const BOT_ID = 'USLACKBOT';
+    const COMMAND_WORD = 'чотач';
+
     public function process()
     {
         $user = $this->get('user_name');
         $user_id = $this->get('user_id');
         $text = $this->get('text');
+
+        if (empty($user) || empty($user_id) || empty($text)) {
+            throw new ErrorApi(ErrorApi::MISSING_INPUT);
+        }
 
         $this->db->insert('slack_log', array(
             'user_id' => $user_id,
@@ -19,15 +26,24 @@ class ApiSlack extends ApiAbstract
 
         $this->set_success(true);
 
-        if ($user == 'slackbot') {
+        // Ignore slackbot himself
+        if ($user_id == self::BOT_ID) {
             return;
         }
 
-        $this->add_answer('text', $user);
+        // Check for commands
+        if (preg_match('/^' . self::COMMAND_WORD . '\b\s*(.*)/i', $text, $command)) {
+            $this->add_answer('text', $this->process_command($command));
+        }
     }
 
     protected function is_link($text)
     {
-        return false;
+        return strpos($text, '<') !== false;
+    }
+
+    protected function process_command($command)
+    {
+        return $command;
     }
 }
