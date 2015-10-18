3129c90c04aaba77eeb780c41f62f596ee28b8e9
Konstantin Pakshaev <derinov@odesk.com>
2015-10-18 12:56:48 +0200

    slack api basic command processing

--- libs/Api/Slack.php
+++ libs/Api/Slack.php
@@ -32,8 +32,8 @@ class ApiSlack extends ApiAbstract
         }
 
         // Check for commands
-        if (preg_match('/^' . self::COMMAND_WORD . '\b\s*(.*)/i', $text, $command)) {
-            $this->add_answer('text', $this->process_command($command));
+        if (preg_match('/^' . self::COMMAND_WORD . '\W\s*(.*)/i', $text, $command)) {
+            $this->add_answer('text', $this->process_command($command[1]));
         }
     }
 
@@ -44,6 +44,52 @@ class ApiSlack extends ApiAbstract
 
     protected function process_command($command)
     {
-        return $command;
+        if (empty($command)) {
+            return $this->do_info();
+        }
+
+        if (!preg_match('/(\w+)\W\s*(.*)/i', $command, $match)) {
+            return '';
+        }
+
+        $type = $match[1];
+        $params = preg_split('\s+', $match[2]);
+
+        switch ($type) {
+            case "инфо": return $this->do_info($params);
+            case "найди": return $this->do_search($params);
+            case "добавь": return $this->do_add($params);
+            default: return '';
+        }
+    }
+
+    protected function do_info($params = array())
+    {
+        if (empty($params)) {
+            return "Вас приветствует бакабот. \n".
+            "Для получения справки по команде напишите " . self::COMMAND_WORD . " инфо {имя команды}. \n" .
+            "Доступные команды: найди, добавь";
+        }
+
+        $result = array();
+        foreach ($params as $param) {
+            switch ($param) {
+                case "найди": $result[] = "To be described"; break;
+                case "добавь": $result[] = "To be described"; break;
+                default: $result[] = "Неизвестная команда $param"; break;
+            }
+        }
+
+        return implode("\n", $result);
+    }
+
+    protected function do_search($params)
+    {
+        return serialize($params);
+    }
+
+    protected function do_add($params)
+    {
+        return serialize($params);
     }
 }
