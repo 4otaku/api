119b81d92d697a7761b333d3d1fbd2d9991218e7
Konstantin Pakshaev <derinov@odesk.com>
2015-10-27 23:26:38 +0100

    28: order and limit in find slack command

--- libs/Slack/Command/Enginfo.php
+++ libs/Slack/Command/Enginfo.php
@@ -9,8 +9,8 @@ class SlackCommandEnginfo extends SlackCommandInfo
         $result =  parent::process($params);
 
         return str_replace(
-            array('пачи', 'инфо', 'найди', 'добавь', 'случайный', 'покажи'),
-            array('pachi', 'info', 'find', 'add', 'random', 'show'),
+            array('пачи', 'инфо', 'найди', 'добавь', 'случайный', 'покажи', 'случайное', 'лучшее', 'юри'),
+            array('pachi', 'info', 'find', 'add', 'random', 'show', 'random', 'best', 'yuri'),
             $result);
     }
 }
--- libs/Slack/Command/Search.php
+++ libs/Slack/Command/Search.php
@@ -6,6 +6,15 @@ class SlackCommandSearch extends SlackCommandList
 {
     protected $per_page = 3;
 
+    protected function process($params)
+    {
+        while(!empty($params) && $this->check_extra_condition($params[0])) {
+            array_shift($params);
+        }
+
+        return parent::process($params);
+    }
+
     protected function format_result($data)
     {
         $result = "Всего по этому запросу есть $data[count] артов";
@@ -14,5 +23,28 @@ class SlackCommandSearch extends SlackCommandList
                 "http://images.4otaku.org/art/$art[md5]_largethumb.jpg";
         }
         return $result;
+    }
+
+    protected function check_extra_condition($value)
+    {
+        switch ($value) {
+            case is_numeric($value):
+                $value = (int) $value;
+                if ($value > 0 && $value < 10) {
+                    $this->per_page = $value;
+                    return true;
+                }
+                return false;
+            case 'случайное':
+            case 'random':
+                $this->sort = 'random';
+                return true;
+            case 'лучшее':
+            case 'best':
+                $this->sort = 'rating';
+                return true;
+            default:
+                return false;
+        }
     }
 }
